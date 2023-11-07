bd7d4286647aba7ac14e614c3ad763858c805435
Reno0vacio <horkrux78@gmail.com>
2023-11-07 17:53:31 +0100

    Második feladat

--- ts.js
+++ ts.js
@@ -15,7 +15,7 @@ function legkisebbHengerurtartalom(autoTomb) {
     }
     return "A legkisebb henger\u00FCrtartalommmal rendelkez\u0151 aut\u00F3 ".concat(autoNeve, ", \u00E9s ").concat(leggkisebbHenger, " a henger\u00FCrtartalma!");
 }
-console.log(legkisebbHengerurtartalom(autok));
+//console.log(legkisebbHengerurtartalom(autok));
 function benzinesekSzama(autoTomb) {
     var benzinesAutokSzama = 0;
     for (var _i = 0, autoTomb_1 = autoTomb; _i < autoTomb_1.length; _i++) {
@@ -26,4 +26,44 @@ function benzinesekSzama(autoTomb) {
     }
     return benzinesAutokSzama;
 }
-console.log(benzinesekSzama(autok));
+//console.log(benzinesekSzama(autok));
+function atlatHenger(autoTomb) {
+    var osszHenger = 0;
+    var hengerekSzama = 0;
+    for (var _i = 0, autoTomb_2 = autoTomb; _i < autoTomb_2.length; _i++) {
+        var autohenger = autoTomb_2[_i];
+        osszHenger += autohenger.hengerurtartalom;
+        hengerekSzama++;
+    }
+    return osszHenger / hengerekSzama;
+}
+//console.log(atlatHenger(autok));
+function vanEBenzines(autoTomb) {
+    var benzinesAutokSzama = 0;
+    for (var _i = 0, autoTomb_3 = autoTomb; _i < autoTomb_3.length; _i++) {
+        var autohenger = autoTomb_3[_i];
+        if (autohenger.benzinesE) {
+            benzinesAutokSzama++;
+        }
+    }
+    if (benzinesAutokSzama === 0) {
+        console.log("Nem volt benzines auto!");
+    }
+    return benzinesAutokSzama;
+}
+//console.log(vanEBenzines(autok));
+function benzinesValogato(autoTomb) {
+    var nemBenzines = [];
+    var benzines = [];
+    for (var _i = 0, autoTomb_4 = autoTomb; _i < autoTomb_4.length; _i++) {
+        var auto = autoTomb_4[_i];
+        if (auto.benzinesE) {
+            benzines.push(auto);
+        }
+        else {
+            nemBenzines.push(auto);
+        }
+    }
+    return "A benzines autok szama ".concat(benzines.length, ", \u00E9s a nem benzinesek szama ").concat(nemBenzines.length, ".");
+}
+console.log(benzinesValogato(autok));
--- ts.ts
+++ ts.ts
@@ -1,4 +1,9 @@
-let autok: {type: string, hengerurtartalom: number, benzinesE: boolean} [] = [
+interface Autok {
+    type: string, 
+    hengerurtartalom: number,
+     benzinesE: boolean
+    }
+let autok: Autok [] = [
     {type: "toyota", hengerurtartalom: 5000, benzinesE: false},
     {type: "toyota", hengerurtartalom: 1889, benzinesE: true},
      {type: "reno", hengerurtartalom: 2780, benzinesE: true},
@@ -14,7 +19,7 @@ function legkisebbHengerurtartalom(autoTomb: {type:string, hengerurtartalom:numb
         }
     }  return `A legkisebb hengerürtartalommmal rendelkező autó ${autoNeve}, és ${leggkisebbHenger} a hengerürtartalma!`
 }
-console.log(legkisebbHengerurtartalom(autok));
+//console.log(legkisebbHengerurtartalom(autok));
 
 function benzinesekSzama(autoTomb: {type:string, hengerurtartalom:number, benzinesE: boolean}[]) {
     let benzinesAutokSzama: number = 0;
@@ -25,4 +30,45 @@ function benzinesekSzama(autoTomb: {type:string, hengerurtartalom:number, benzin
     }
     return benzinesAutokSzama
 }
-console.log(benzinesekSzama(autok));
+//console.log(benzinesekSzama(autok));
+
+function atlatHenger (autoTomb: {type:string, hengerurtartalom:number, benzinesE: boolean}[]) {
+    let osszHenger = 0;
+    let hengerekSzama = 0;
+    for (let autohenger of autoTomb) {
+        osszHenger += autohenger.hengerurtartalom;
+        hengerekSzama++ 
+    }
+    return osszHenger / hengerekSzama
+}
+//console.log(atlatHenger(autok));
+
+function vanEBenzines (autoTomb: {type:string, hengerurtartalom:number, benzinesE: boolean}[]) {
+    let benzinesAutokSzama = 0;
+    for (let autohenger of autoTomb) {
+        if (autohenger.benzinesE) {
+            benzinesAutokSzama++
+
+        }
+    }
+    if (benzinesAutokSzama === 0) {
+        console.log("Nem volt benzines auto!")
+    }
+    return benzinesAutokSzama
+}
+//console.log(vanEBenzines(autok));
+
+function benzinesValogato (autoTomb: {type:string, hengerurtartalom:number, benzinesE: boolean}[]) {
+    let nemBenzines: Autok[] = [];
+    let benzines: Autok[] = [];
+    for (let auto of autoTomb) {
+        if (auto.benzinesE) {
+            benzines.push(auto)
+            
+        } else {
+            nemBenzines.push(auto)
+        }
+    }
+    return `A benzines autok szama ${benzines.length}, és a nem benzinesek szama ${nemBenzines.length}.`
+}
+console.log(benzinesValogato(autok));
